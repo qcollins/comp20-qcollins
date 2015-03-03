df229ad3acf1b61dc90d882decb4705a9e3c15db
Quinn Collins <Quinn.Collins@tufts.edu>
2015-03-02 22:03:36 -0500

    updated game.js and added readme

---
+++ mspacman/README.md
@@ -0,0 +1,6 @@
+Quinn Collins
+3/2/2015
+
+-all aspects of the lab have been correctly implemented
+-I worked alone on this assignment
+-I spent ~2 hours on this lab
--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,13 @@
-// Replace this with your code...
+function init()
+{
+	canvas = document.getElementById('game_canvas');
+	ctx = canvas.getContext('2d');
+	var img = new Image();
+	img.addEventListener("load", function() {
+	ctx.drawImage(img, 322, 0, 785, 137, 0, 0, 785, 137);
+	ctx.drawImage(img, 83 ,23, 13, 14, 36, 31, 13, 14);
+	}, false);
+	img.src="pacman10-hp-sprite.png";
+	
+	//-463, -137
+}
