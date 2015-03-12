5928b03d9aa7139bd28dcedf9dbc83a7850a7a52
Quinn Collins <Quinn.Collins@tufts.edu>
2015-03-12 00:49:30 -0400

    updated lab.js

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,28 @@
-// Your JavaScript goes here...
+function parse()
+{
+	var xmlhttp = new XMLHttpRequest();
+
+	xmlhttp.onreadystatechange = function()
+	{
+    	if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
+    	{
+        	var toprint = JSON.parse(xmlhttp.responseText);
+        	make(toprint);
+   		}
+	}
+	xmlhttp.open("GET", "data.json", true);
+	xmlhttp.send();
+
+	function make(data) 
+	{
+    	var out = "";
+    	var i=0;
+
+    	for(i; i < data.length; i++) 
+    	{
+        	out += data[i].content;
+        	out += data[i].username;
+    	}
+    	document.getElementById("messages").innerHTML = out;
+	}
+}
